10871f4d7a96a583a9e3b835f5ea5711c2a832f4
Daniel Alexander Cano Cuartas <dacanoc@unal.edu.co>
2021-09-16 21:17:34 -0500

    created import.py to import csv into database

---
+++ import.py
@@ -0,0 +1,15 @@
+import pdb
+from models import db
+from sqlalchemy import create_engine
+from app import app
+import pandas as pd
+
+sql_engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=False)
+connection = sql_engine.raw_connection()
+
+if __name__ == "__main__":
+
+    column_names =  ["id", "name", "type1", "type2", "total", "hp", "attack", "defense", "sp_attack", "sp_defense", "speed", "generation", "legendary"]
+    with open('pokemon.csv') as csv_file:
+        df = pd.read_csv(csv_file, header = 0, names = column_names, index_col ="id")
+    df.to_sql('pokemon', con=connection, if_exists='replace')
